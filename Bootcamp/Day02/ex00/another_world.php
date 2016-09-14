cccb77a12cce0ac054a12488355f5a46a7a52687
Matthew Williams <FatBat@Matthews-iMac.local>
2016-09-14 20:45:16 +0200

    added alternate method of cleaning string

--- Bootcamp/Day02/ex00/another_world.php
+++ Bootcamp/Day02/ex00/another_world.php
@@ -5,7 +5,8 @@
 
 	if ($argc == 1)
 		exit(0);
-	$str = preg_replace("/\s+/", " ", $argv[1]);
+	//$str = preg_replace("/\s+/", " ", $argv[1]);
+	$str = preg_replace("/\s+/S", " ", $argv[1]);
 	$str = trim($str);
 	print "$str\n";
 
