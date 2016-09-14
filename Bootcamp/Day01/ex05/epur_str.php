27d48b9cb3776437ca6c203e80adaace49761c4d
Matthew VAN WYK <mvanwyk@e5r1p9.jnb.42.fr>
2016-09-14 14:02:21 +0200

    Day01 ex05 done

--- Bootcamp/Day01/ex05/epur_str.php
+++ Bootcamp/Day01/ex05/epur_str.php
@@ -1,13 +1,34 @@
 #!/usr/bin/php
 <?php
 
+	$expstr;
+	$outstr;
+	$cnt;
+	$len;
+
 	if ($argc == 1)
 		exit(0);
-	foreach ($argv as $arg)
+	$cnt = 0;
+	$len = 0;
+	$expstr = explode(' ', $argv[1]);
+	$outstr = NULL;
+	foreach ($expstr as $arit)
 	{
-		//explode
-		//clean
-		//print
+		if ($arit == '' || $arit == ' ')
+		{
+			unset($expstr[$cnt]);
+		}
+		$cnt++;
 	}
+	$len = count($expstr);
+	$cnt = 1;
+	foreach ($expstr as $arit)
+	{
+		$outstr .= $arit;
+		if ($cnt < $len)
+			$outstr .= " ";
+		$cnt++;
+	}
+	print $outstr;
 
 ?>
