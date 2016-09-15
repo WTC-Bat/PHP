ba7a4922b33d3b5417cd0a860c0b321c313d9b99
Matthew VAN WYK <mvanwyk@e5r2p7.jnb.42.fr>
2016-09-15 18:25:08 +0200

    magnifying_glass sort of works

--- Bootcamp/Day02/ex02/magnifying_glass.php
+++ Bootcamp/Day02/ex02/magnifying_glass.php
@@ -1,12 +1,36 @@
 #!/usr/bin/php
 <?php
 
-	$html;
+$html;
+$dom;
 
-	if (argc == 1)
-		exit(0);
-	if (is_readable($argv[1]) == FALSE)
-		exit(1);
-	$html = get_file_contents($argv[1])
+if (argc == 1)
+	exit(0);
+if (is_readable($argv[1]) == FALSE)
+	exit(1);
+$html = file_get_contents($argv[1]);
+/*  "(.*)" <-regex for strings surrounded by "  */
+$dom = new DOMDocument;
+$dom->loadHTML($html);
+foreach ($dom->getElementsByTagName("*") as $node)
+{
+	//print "$node->localName\n";
+	if ($node->localName == "a")
+	{
+		$node->nodeValue = strtoupper($node->nodeValue);
+	}
+	if ($node->hasAttribute("title") == TRUE)
+	{
+		foreach ($node->attributes as $att)
+		{
+			if ($att->name == "title")
+			{
+				$att->value = strtoupper($att->value);
+			}
+		}
+	}
+}
+$new_html = $dom->saveHTML();
+print $new_html;
 
 ?>
