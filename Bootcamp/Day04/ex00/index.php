5ded8c2ffb12515036f35cca757079d944c66912
Matthew <myshorts.mail@gmail.com>
2016-09-19 01:16:49 +0200

    Day01/ex00 done

--- Bootcamp/Day04/ex00/index.php
+++ Bootcamp/Day04/ex00/index.php
@@ -1,30 +1,31 @@
+<?php
+session_start();
+if ($_GET["submit"] == "OK")
+{
+	if (!$_GET["login"])
+	{
+		echo "Please enter a valid Login.\n";
+	}
+	else if (!$_GET["passwd"])
+	{
+		echo "Please enter a valid Password.\n";
+	}
+	else
+	{
+		$_SESSION["login"] = $_GET["login"];
+		$_SESSION["passwd"] = $_GET["passwd"];
+	}
+}
+?>
 <html>
 	<head>
 		<title>Ex00 - index.php</title>
 	</head>
 	<body>
 		<form>
-			<input name="login" value="<?PHP echo $S_SESSION['login'];?>" />
-			<input name="passwd" value="<?PHP echo $_SESSION['passwd'];?>" />
+			<p>Login: <input name="login" value="<?PHP echo $_SESSION['login'];?>" /></p>
+			<p>Password: <input name="passwd" value="<?PHP echo $_SESSION['passwd'];?>" /></p>
 			<input type="submit" name="submit" value="OK" />
 		</form>
 	</body>
 </html>
-<?php
-session_start();
-if ($_GET["submit"] == "OK")
-{
-	// if (!$_GET["login"])
-	// {
-	// 	echo "Please enter a valid Login.\n";
-	// 	exit();//?
-	// }
-	// if (!$_GET["passwd"])
-	// {
-	// 	echo "Please enter a valid password.\n";
-	// 	exit();//?
-	// }
-	$_SESSION["login"] = $_GET["login"];
-	$_SESSION["passwd"] = $_GET["passwd"];
-}
-?>
