c7afb1368253c08710fe3f9670fe58c0c29ecd71
Matthew VAN WYK <mvanwyk@e5r4p3.jnb.42.fr>
2016-09-18 17:20:50 +0200

    renamed some commented functions

--- Bootcamp/Rush00/funcs.php
+++ Bootcamp/Rush00/funcs.php
@@ -114,7 +114,7 @@ function clear_file($filepath)
 **	$filename	-	$file to append user to (eg. ../private/passwd)
 */
 /*
-function user_exists($user, $filename)
+function user_exists_arr($user, $filename)
 {
 	$file;
 	$line;
@@ -165,7 +165,7 @@ function user_exists($user_name, $filepath)
 **	$filename	-	$file to append user to (eg. ../private/passwd)
 */
 /*
-function add_user($user, $filepath)	//$user is array
+function add_user_arr($user, $filepath)	//$user is array
 {
 	$file;
 
@@ -205,27 +205,14 @@ function add_user($login, $passwd, $filepath)
 		file_put_contents($filepath, $usercsv, FILE_APPEND);
 	else
 		echo "User already exists!\n";
-	/*
-	if (file_exists($filepath))
-	{
-		$hashed = hash("whirlpool", $passwd);
-		$usercsv = $login . "," . $hashed . "\n";
-		if (user_exists($login, $filepath) == FALSE)
-			file_put_contents($filepath, $usercsv, FILE_APPEND);
-		else
-			echo "User already exists!\n";
-	}
-	*/
 }
 
 function remove_user($user_name)
 {
 	$users;
 
-	//if (file_exists("../private/passwd"))		//Assumes passwd is a csv file
-	if (file_exists("./private/passwd"))
+	if (file_exists("./private/passwd"))	//Assumes passwd is a csv file
 	{
-		//$users = get_users("../private/passwd");
 		$users = get_users("./private/passwd");
 	}
 	else
@@ -234,13 +221,8 @@ function remove_user($user_name)
 	}
 	foreach ($users as $u)
 	{
-		//echo "Unsetting: \n";
-		//echo $u["user_name"] . ": ";
-		//echo $u["pwhash"] . "\n";
-		unset($u);
-		//echo "User Removed\n";
-		//echo "User: " . $u["user_name"] . "\n";
-		//echo "Hash: " . $u["pwhash"] . "\n";
+		if ($user_name == $u[0]);
+			unset($u);
 	}
 	clear_file("./private/passwd");
 	foreach ($users as $u)
