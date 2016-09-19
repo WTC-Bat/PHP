49ea4a11ab35daf0f3e0cc86de96e46c68579d9a
Matthew <myshorts.mail@gmail.com>
2016-09-19 22:31:56 +0200

    Day05/ex03 done

--- Bootcamp/Day05/ex01/ex01.sql
+++ Bootcamp/Day05/ex01/ex01.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ft_table
 (
 	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	login char(8) DEFAULT "toto" NOT NULL,
+	login char(8) DEFAULT 'toto',
 	group ENUM('staff', 'student', 'other') NOT NULL,
 	creation_date DATE NOT NULL
 );
