669e69ea18b9f72a9c9f7dec43ed3b855c3e866f
Matthew <myshorts.mail@gmail.com>
2016-09-25 12:33:10 +0200

    fixed select

--- Bootcamp/Day09/ex04/select.php
+++ Bootcamp/Day09/ex04/select.php
@@ -27,27 +27,26 @@
 // 	}
 // 	// return ("ERR");
 // }
-
-$file;
-$line;
-$csv;
-$csvar = array();
-$html;
-$csvex;
-
 if (file_exists("list.csv"))
 {
+	$file;
+	$line;
+	$csv;
+	$csvar = array();
+	$html;
+	// $csvex = array();
+
 	$file = fopen("list.csv", "r");
 	if ($file)
 	{
 		$html = "";
 		while (($line = fgets($file)) == TRUE)
 		{
-			$csv = str_getcsv($line);
-			$csvex = explode(";", $csv);
+			$csv = str_getcsv($line, ";");
+			// $csvex = explode(";", $csv);
 			// print("<div>" . $csv[1] . "</div>");
 			//print("<div id=\"todo\">" . $csv[1] . "</div>");
-			$html .= "<div id=\"todo\">" . $csvex[1] . "</div>";
+			$html .= "<div id=\"todo\">" . $csv[1] . "</div>";
 		}
 		// return ("HUH");
 		echo($html);
--- Bootcamp/Day09/ex04/todo.js
+++ Bootcamp/Day09/ex04/todo.js
@@ -1,16 +1,27 @@
 $(document.body).ready(function()
 {
-	$.ajax
-	({
-  		url: 'select.php',
-		dataType: 'html',
-  		success: function(data)
+	// $.ajax
+	// ({
+ //  		url: 'select.php',
+	// 	dataType: 'html',
+ //  		success: function(data)
+	// 	{
+    // 		console.log("Data: " + data);
+ //  		},
+	// 	error: function(data)
+	// 	{
+	// 		console.log("Error");
+	// 	}
+	// });
+	var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function()
+	{
+        if (this.readyState == 4 && this.status == 200)
 		{
-    		console.log("Data: " + data);
-  		},
-		error: function(data)
-		{
-			console.log("Error");
-		}
-	});
+            // console.log(this.responseText);
+			$("#ft_list").append(this.responseText);
+        }
+    };
+    xmlhttp.open("POST", "select.php?select=OK", true);
+    xmlhttp.send();
 });
